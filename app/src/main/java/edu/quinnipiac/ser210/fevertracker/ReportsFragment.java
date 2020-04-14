59293a2caec0568f585fdc345adbfd66188e7184
tjcj211 <timcarta@hotmail.com>
2020-04-14 14:16:19 -0400

    Added Reports Screen

---
+++ app/src/main/java/edu/quinnipiac/ser210/fevertracker/ReportsFragment.java
@@ -0,0 +1,29 @@
+package edu.quinnipiac.ser210.fevertracker;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ReportsFragment extends Fragment {
+    public ReportsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reports, container, false);
+    }
+}

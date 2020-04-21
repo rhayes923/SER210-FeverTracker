0db12d81b6fe05f55193510abf684b0861519197
tjcj211 <timcarta@hotmail.com>
2020-04-21 15:01:06 -0400

    Made data show in ListView rather than Spinner.

--- app/src/main/java/edu/quinnipiac/ser210/fevertracker/ReportsFragment.java
+++ app/src/main/java/edu/quinnipiac/ser210/fevertracker/ReportsFragment.java
@@ -1,5 +1,10 @@
 package edu.quinnipiac.ser210.fevertracker;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,15 +14,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 
 public class ReportsFragment extends Fragment {
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     private Spinner spinner;
+    private ListView listReports;
     private TextView temp, date, time;
 
     public ReportsFragment() {
@@ -34,10 +45,13 @@ public class ReportsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_reports, container, false);
         spinner = view.findViewById(R.id.spnrReports);
+        listReports = view.findViewById(R.id.listReports);
         temp = view.findViewById(R.id.txtTemp);
         date = view.findViewById(R.id.txtDate);
         time = view.findViewById(R.id.txtTime);
-        loadSpinnerData();
+
+        //loadSpinnerData();
+        loadListData();
         // Inflate the layout for this fragment
         return view;
     }
@@ -62,6 +76,24 @@ public class ReportsFragment extends Fragment {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {}
+        });
+    }
+
+    private void loadListData() {
+        DatabaseHelper db = new DatabaseHelper(getContext());
+        List<String> reports = db.getAllReports();
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_list_item_1, reports);
+        listReports.setAdapter(dataAdapter);
+        listReports.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                //String value = listReports.getSelectedItem().toString();
+                String value = Integer.toString(position+1);
+                List<String> info = MainActivity.displayInfo(value);
+                temp.setText("Temperature: " + info.get(0));
+                date.setText("Date: " + info.get(1));
+                time.setText("Time: " + info.get(2));
+            }
         });
     }
 }
